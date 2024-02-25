70f339b82c1de0788f4682b7c306316c72c8167d
Nerea <neresaas@gmail.com>
2024-02-25 12:56:29 +0100

    post /permissions

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let express = require ('express');
 
 let app = express();
 let port = 8081;
+// Lee datos del body que estén en formato json
+app.use(express.json());
 
 let routerPerissions = require('./routers/routerPermissions');
 app.use('/permissions', routerPerissions);
--- routers/routerPermissions.js
+++ routers/routerPermissions.js
@@ -7,4 +7,20 @@ routerPerissions.get('/', (req, res) => {
     res.json(permissions)
 });
 
+routerPerissions.post('/', (req, res) => {
+    let text = req.body.text
+    let userId = req.body.userId
+
+    let lastId = permissions[permissions.length - 1].id
+
+    permissions.push({
+        id: lastId + 1,
+        text: text,
+        approbedBy: [],
+        userId: userId
+    })
+    // Para devolver un JSON tiene que ser un objeto, una clave y un valor ------> 1 { clave: valor }
+    res.json({ id: lastId + 1 })
+});
+
 module.exports = routerPerissions;
